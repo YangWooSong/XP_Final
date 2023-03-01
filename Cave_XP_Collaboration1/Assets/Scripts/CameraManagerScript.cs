d2939a9e6a8d5d8f14462a51e84cc52b61a6a2a7
YangWooSong <olivia6844@naver.com>
2023-03-01 23:57:25 +0900

    CameraZoom

--- Cave_XP_Collaboration1/Assets/Scripts/CameraManagerScript.cs
+++ Cave_XP_Collaboration1/Assets/Scripts/CameraManagerScript.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.EventSystems;
 public class CameraManagerScript : MonoBehaviour
 {
     public Camera mainCam;
-    private GameObject telposGameObj;
+    private GameObject telposGameObj1;
+    private GameObject telposGameObj2;
+    private GameObject telposGameObj3;
     private GameObject zoomObj;
     private GameObject backZoomBtn;
-    private GameObject cameraDefaultPos;
+    private GameObject cameraDefaultPos1;
+    private GameObject cameraDefaultPos2;
 
     private void Awake()
     {
@@ -37,18 +40,25 @@ public class CameraManagerScript : MonoBehaviour
     void Start()
     {
         mainCam = Camera.main;
-        telposGameObj = GameObject.Find("camera_ZoomPos");
-        zoomObj = GameObject.Find("zoom_click");
+        telposGameObj1 = GameObject.Find("camera_ZoomPos1");
+        telposGameObj2 = GameObject.Find("camera_ZoomPos2");
+        zoomObj = GameObject.Find("ZoomClick");
         backZoomBtn = GameObject.Find("zoomBackBtn");
         if(backZoomBtn == null){}
         else backZoomBtn.SetActive(false);
-        cameraDefaultPos = GameObject.Find("camera_defaultPos");
+        cameraDefaultPos1 = GameObject.Find("camera_defaultPos");
+        if (GameObject.Find("CameraPosition1"))
+        {
+            cameraDefaultPos2 = GameObject.Find("CameraPosition1");
+            telposGameObj3 = GameObject.Find("camera_ZoomPos3");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -57,10 +67,25 @@ public class CameraManagerScript : MonoBehaviour
                 //Debug.Log(hit.transform.gameObject);
                 if (hit.transform.gameObject.tag=="telpos")
                 {
+                    if (hit.transform.GetSiblingIndex() == 0)
+                    {
+                        mainCam.gameObject.transform.position = telposGameObj1.transform.position;
+                        mainCam.gameObject.transform.rotation = telposGameObj1.transform.rotation;
+                    }
+                    else if(hit.transform.GetSiblingIndex() == 1)
+                    {
+                        mainCam.gameObject.transform.position = telposGameObj2.transform.position;
+                        mainCam.gameObject.transform.rotation = telposGameObj2.transform.rotation;
+                    }
+                    else if (hit.transform.GetSiblingIndex() == 2)
+                    {
+                        mainCam.gameObject.transform.position = telposGameObj3.transform.position;
+                        mainCam.gameObject.transform.rotation = telposGameObj3.transform.rotation;
+                        cameraDefaultPos1 = cameraDefaultPos2;
+                    }
                     backZoomBtn.SetActive(true);
                     zoomObj.SetActive(false);
-                    mainCam.gameObject.transform.position = telposGameObj.transform.position;
-                    mainCam.gameObject.transform.rotation = telposGameObj.transform.rotation;
+                    
                     Debug.Log("¼º°ø");
 
                 }
@@ -71,8 +96,8 @@ public class CameraManagerScript : MonoBehaviour
     {
         zoomObj.SetActive(true);
         backZoomBtn.SetActive(false);
-        mainCam.gameObject.transform.position = cameraDefaultPos.transform.position;
-        mainCam.gameObject.transform.rotation = cameraDefaultPos.transform.rotation;
+        mainCam.gameObject.transform.position = cameraDefaultPos1.transform.position;
+        mainCam.gameObject.transform.rotation = cameraDefaultPos1.transform.rotation;
 
     }
 }
